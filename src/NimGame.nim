94537bdc9fde321bf5c69bd93bd3f85e2870fc5b
komarovec <komarovec1@gmail.com>
2020-07-22 13:15:05 +0200

    Game mechanics
    Game is now actually playable, goal, difficulty system, physics tweaks.

--- src/NimGame.nim
+++ src/NimGame.nim
@@ -11,13 +11,17 @@ window.framerate_limit = 200
 # Procedures declaration
 proc calculatePhysics()
 proc handleEvents(event: Event, deltaTime: float)
+proc respawnBox()
+proc restartGame()
 
 # Help vars/objs
 var 
   clock = newClock()
   lastMousePos = vec2(0.0,0.0)
   wallTexture = new_Texture("resources/wall.jpg")
-  separatorTexture = new_Texture("resources/separator.png")
+  difficulty = 1
+  difficultyOffset = 1
+  towerHeight = 0
 
 # Constants
 const
@@ -25,9 +29,12 @@ const
   GRAVITY_FORCE = -5
 
 # Objects
-var boxes = @[newBox(vec2(100,300), 0.3),newBox(vec2(600,300), 0.3),newBox(vec2(1000,300), 0.3),newBox(vec2(1200,300), 0.3)]
+var boxes: seq[Box] = @[]
+var sepa = newSeparator(1, window.size)
+var baseBox: Box
 
-
+# Start game logic
+restartGame()
 
 #[
 #---------------------------                       ---------------------------#
@@ -55,10 +62,7 @@ while window.open:
   wall.destroy()
 
   # Create separator
-  let separator = new_Sprite(separatorTexture)
-  separator.position = vec2(window.size.x/2 - separator.texture.size.x.float/2, window.size.y.float - separator.texture.size.y.float)
-  window.draw separator
-  separator.destroy()
+  sepa.paint(window)
 
   # Paint boxes
   for box in boxes.items:
@@ -75,29 +79,76 @@ window.destroy()
 #---------------------------                       ---------------------------#
 ]#
 
+# Respawn box
+proc respawnBox() =
+  if(boxes.len >= 3):
+    for box in boxes.items:
+      box.isStatic = true
+    boxes[^1].isStatic = false
+    boxes[^2].isStatic = false
+  boxes.add(newBox(vec2(100,-300), 1/(difficulty+difficultyOffset)))
+
+# Restart box
+proc restartGame() =
+  # Objects
+  boxes = @[]
+  respawnBox()
+
+  # Base Box
+  baseBox = newBox(vec2(((sepa.pos.x.float + sepa.size.x.float + window.size.x.float)/2 - boxes[0].size.x/2).int, window.size.y - boxes[0].size.y - 1), 1/(difficulty+difficultyOffset))
+  baseBox.isTower = true
+  baseBox.isTopTower = true
+  baseBox.isBase = true
+  boxes.add(baseBox)
+
+  towerHeight = baseBox.size.y
+
 # Does physics? somehow...
 proc calculatePhysics() =
+  var toDelete: seq[int] = @[]
+  var spawnNewBox = false
+  var restartGame = false
+
   # Box physics
-  for box in boxes.items:
+  for boxi, box in boxes.pairs:
     # Gravity
     if(not (box.dragged or box.grounded)):
       box.velocity.y += GRAVITY_FORCE
 
     # Screen borders
     if((box.pos.x+box.size.x.float) >  window.size.x.float):
+      if(box.isThrown):
+        if(box.isTower):
+          restartGame = true
+        else:
+          toDelete.add(boxi)
       box.pos.x =  (window.size.x-box.size.x).float
       box.velocity.x = -(box.velocity.x*box.bounce)
     if((box.pos.y+box.size.y.float) >  window.size.y.float):
+      if(box.isThrown):
+        if(box.isTower and not box.isBase):
+          restartGame = true
+        else:
+          toDelete.add(boxi)
       box.pos.y =  (window.size.y-box.size.y).float
       box.velocity.y = -(box.velocity.y*box.bounce)
       box.velocity.x = box.velocity.x*box.bounce
     if(box.pos.x < 0):
+      if(box.isThrown):
+        if(box.isTower):
+          restartGame = true
+        else:
+          toDelete.add(boxi)
       box.pos.x = 0
       box.velocity.x = -(box.velocity.x*box.bounce)
     #[if(box.pos.y < 0): # TOP BORDER
       box.pos.y = 0
       box.velocity.y = -(box.velocity.y*box.bounce)]#
-    
+
+    # Separator collision
+    if(sepa.boxCollision(box)):
+      toDelete.add(boxi)
+
     # Box collisions
     for box2 in boxes.items:
       if(box == box2): continue
@@ -152,6 +203,47 @@ proc calculatePhysics() =
         box.velocity.y = (1-box.bounce)*pomyVel2 - box.bounce*pomyVel1
         box2.velocity.y = (1-box.bounce)*pomyVel1 - box.bounce*pomyVel2
 
+        # Flag tower if static and on top
+        if(not (box.isTower and box2.isTower)):
+          if(box.isTopTower and abs(box2.velocity.x) + abs(box2.velocity.y) < 5):
+            box2.isTower = true
+            box2.isTopTower = true
+            box.isTopTower = false
+            spawnNewBox = true
+            # Check if end game -> increase difficulty
+            towerHeight += box2.size.y
+            if(towerHeight > sepa.size.y):
+              inc(difficulty)
+              restartGame = true
+
+          elif(box2.isTopTower and abs(box.velocity.x) + abs(box.velocity.y) < 5):
+            box.isTower = true
+            box.isTopTower = true
+            box2.isTopTower = false
+            spawnNewBox = true
+            # Check if end game -> increase difficulty
+            towerHeight += box.size.y
+            if(towerHeight > sepa.size.y):
+              inc(difficulty)
+              restartGame = true
+
+          # If box landed not on top, restart game
+          if(not(box.isTopTower or box2.isTopTower)):
+            restartGame = true
+
+  # Delete tagged boxes
+  for boxi in toDelete:
+    boxes.delete(boxi)
+    respawnBox()
+
+  # Spawn new box
+  if(spawnNewBox):
+    respawnBox()
+
+  # Restart
+  if(restartGame):
+    restartGame()
+
 # Handle user inputs
 proc handleEvents(event: Event, deltaTime: float) =
   if event.kind == EventType.Closed:
@@ -161,6 +253,7 @@ proc handleEvents(event: Event, deltaTime: float) =
   elif event.kind == EventType.MouseButtonPressed:
     for box in boxes.items:
       if(box.collision(vec2(event.mouseButton.x,event.mouseButton.y))):
+        if(box.isTower or box.isThrown): continue
         box.dragged = true
         box.velocity = vec2(0,0)
         box.draggedPoint = vec2(event.mouseButton.x.float - box.pos.x, event.mouseButton.y.float - box.pos.y)
@@ -179,9 +272,18 @@ proc handleEvents(event: Event, deltaTime: float) =
         box.velocity.x = (distance.x / deltaTime) * THROW_MULTIPLIER
         box.velocity.y = (distance.y / deltaTime) * THROW_MULTIPLIER
 
+        box.isThrown = true
+
   # MouseMove -> Recalculate dragging coords
   elif event.kind == EventType.MouseMoved:
-    for box in boxes.items:
+    var toDelete: seq[int] = @[]
+    for boxi, box in boxes.pairs:
       if(box.dragged):
-        lastMousePos = vec2(box.pos.x + box.draggedPoint.x, box.pos.y + box.draggedPoint.y)
-        box.pos = vec2(event.mouseMove.x.float - box.draggedPoint.x, event.mouseMove.y.float - box.draggedPoint.y)
+        if(event.mouseMove.y.float < sepa.pos.y):
+          toDelete.add(boxi)
+        else:
+          lastMousePos = vec2(box.pos.x + box.draggedPoint.x, box.pos.y + box.draggedPoint.y)
+          box.pos = vec2(event.mouseMove.x.float - box.draggedPoint.x, event.mouseMove.y.float - box.draggedPoint.y)
+    for boxi in toDelete.items:
+      boxes.del(boxi)
+      respawnBox()
--- src/gameObjects.nim
+++ src/gameObjects.nim
@@ -6,6 +6,12 @@ const
 
 # Types
 type
+  Separator* = ref object
+    pos*: Vector2f
+    size*: Vector2i
+    texture*: Texture
+    scale*: float
+
   Box* = ref object
     pos*: Vector2f
     size*: Vector2i
@@ -17,22 +23,63 @@ type
     bounce*: float
     grounded*: bool
     lastPos*: Vector2f
+    isThrown*: bool
+    isBase*: bool
+    isTower*: bool
+    isTopTower*: bool
+    isStatic*: bool
 
+#[
+#---------------------------                       ---------------------------#
+#---------------------------       Separator       ---------------------------#
+#---------------------------                       ---------------------------#
+]#
+# Paint separator
+proc paint*(b: Separator; window: RenderWindow) =
+  let mySepa = new_Sprite(b.texture)
+  mySepa.position = b.pos
+  mySepa.scale = vec2(b.scale,b.scale)
+  window.draw mySepa
+
+  mySepa.destroy()
+
+# Return true if box is in the separator
+proc boxCollision*(s: Separator; b: Box): bool =
+  if(b.pos.x + b.size.x.float > s.pos.x and b.pos.x < s.pos.x + s.size.x.float and b.pos.y + b.size.y.float > s.pos.y and b.pos.y < s.pos.y + s.size.y.float):
+    return true
+  else:
+    return false
+
+# Instantiate new box
+proc newSeparator*(scale: float = 1.0, wsize: Vector2i): Separator =
+  new result
+  result.texture = new_Texture("resources/separator.png")
+  result.size = vec2((result.texture.size.x.float*scale).int, (result.texture.size.y.float*scale).int)
+  result.scale = scale
+  result.pos = vec2(wsize.x/2-result.size.x/2, (wsize.y-result.size.y).float)
+
+
+#[
+#---------------------------                       ---------------------------#
+#---------------------------          Box          ---------------------------#
+#---------------------------                       ---------------------------#
+]#
 # Paint box
 proc paint*(b: Box; window: RenderWindow; dt: float) =
-  let acc = vec2((b.velocity.x),(b.velocity.y))
-  b.lastPos.x = b.pos.x
-  b.lastPos.y = b.pos.y
+  if(not b.isStatic):
+    let acc = vec2((b.velocity.x),(b.velocity.y))
+    b.lastPos.x = b.pos.x
+    b.lastPos.y = b.pos.y
 
-  b.pos.x -= acc.x*dt*VEL_MULTIPLIER
-  b.pos.y -= acc.y*dt*VEL_MULTIPLIER
-  b.velocity.x -= acc.x/100
-  b.velocity.y -= acc.y/100
+    b.pos.x -= acc.x*dt*VEL_MULTIPLIER
+    b.pos.y -= acc.y*dt*VEL_MULTIPLIER
+    b.velocity.x -= acc.x/100
+    b.velocity.y -= acc.y/100
 
-  if(abs(b.lastPos.x - b.pos.x) < 0.01 and abs(b.lastPos.y - b.pos.y) < 0.01 and abs((b.pos.y + b.size.y.float) - window.size.y.float) <= 2):
-    b.grounded = true
-  else:
-    b.grounded = false
+    if(abs(b.lastPos.x - b.pos.x) < 0.01 and abs(b.lastPos.y - b.pos.y) < 0.01 and abs((b.pos.y + b.size.y.float) - window.size.y.float) <= 2):
+      b.grounded = true
+    else:
+      b.grounded = false
 
   #if(abs(b.lastPos.x - b.pos.x) != 0 or abs(b.lastPos.y - b.pos.y) != 0):
   #  echo abs(b.lastPos.x - b.pos.x), " ", abs(b.lastPos.y - b.pos.y)
@@ -65,3 +112,8 @@ proc newBox*(pos: Vector2i, scale: float = 1.0): Box =
   result.bounce = 0.3
   result.grounded = false
   result.lastPos = vec2(0,0)
+  result.isThrown = false
+  result.isTower = false
+  result.isTopTower = false
+  result.isBase = false
+  result.isStatic = false
